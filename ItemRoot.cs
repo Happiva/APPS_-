786faba4612c051429141f671876d0e4d73a5596
dayeondayeon <50194490+dayeondayeon@users.noreply.github.com>
2019-08-13 21:03:10 +0900

    Add files via upload

---
+++ ItemRoot.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class Item
+    {
+        public enum TYPE
+        {
+            NONE = -1,
+            POTATO = 0,
+            CRAB = 1
+        };
+    }
+
+    public class ItemRoot : MonoBehaviour
+    {
+
+        public GameObject Potatoprefab = null;
+        public GameObject Crabprefab = null;
+        public static float RESPAWN_TIME_POTATO = 10.0f;
+        public static float RESPAWN_TIME_CRAB = 15.0f;
+        protected List<Vector3> respawn_points;
+        private float respawn_timer_potato = 0.0f;
+        private float respawn_timer_crab = 0.0f;
+
+        public void respawnPotato()
+        {
+            GameObject go = GameObject.Instantiate(this.Potatoprefab) as GameObject;
+            Vector3 pos = GameObject.Find("potato").transform.position;
+            pos.y = 29.5f;
+            pos.x = Random.Range(400f, 410f);
+            pos.z = Random.Range(340f, 360f);
+            go.transform.position = pos;
+        }
+
+        public void respawnCrab()
+        {
+            GameObject co = GameObject.Instantiate(this.Crabprefab) as GameObject;
+            Vector3 pos = GameObject.Find("Crab").transform.position;
+            pos.y = 28f;
+            pos.x = Random.Range(300f, 330f);
+            pos.z = Random.Range(95f, 125f);
+            co.transform.position = pos;
+        }
+
+        void Start()
+        {
+            this.respawn_points = new List<Vector3>();
+            GameObject[] respawns = GameObject.FindGameObjectsWithTag("Potato");
+            foreach (GameObject go in respawns)
+            {
+                MeshRenderer renderer = go.GetComponentInChildren<MeshRenderer>();
+                if (renderer != null)
+                {
+                    renderer.enabled = false;
+                }
+                this.respawn_points.Add(go.transform.position);
+            }
+        }
+
+        void Update()
+        {
+            respawn_timer_potato += Time.deltaTime;
+            if (respawn_timer_potato > RESPAWN_TIME_POTATO)
+            {
+                respawn_timer_potato = 0.0f;
+                this.respawnPotato();
+            }
+            respawn_timer_crab += Time.deltaTime;
+            if (respawn_timer_crab > RESPAWN_TIME_CRAB)
+            {
+                respawn_timer_crab = 0.0f;
+                this.respawnCrab();
+            }
+        }
+
+
+    }
---
+++ WeatherManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour {
+	public enum Weather { NONE, SUNNY, RAIN, SNOW };
+	public Weather currentWeather;
+
+	public ParticleSystem rain;
+	public ParticleSystem snow;
+
+	[Header("Time Setting")]
+	[Header("Day")]
+
+	public float Weather_time;
+	public int day;
+	private float Origin_time = 10f;
+
+	[Header("LightSetting")] //날씨따라 햇빛강도 다르게//
+	public Light sunLight;
+	private float defaultLightIntensity;
+	public float sunnylightintensity;
+	public float rainlightintensity;
+	public float snowlightintensity;
+
+	public Color defaultLightColor;
+	public Color sunnyColor;
+	public Color rainColor;
+	public Color snowColor;
+
+	private void Start()
+	{
+		currentWeather = Weather.SUNNY;
+		this.defaultLightColor = this.sunLight.color;
+		this.defaultLightIntensity = this.sunLight.intensity;
+		day = 1;
+	}
+
+	public void ChangeWeather(Weather weatherType)
+	{
+		if(weatherType != this.currentWeather)
+		{
+			switch (weatherType)
+			{
+			case Weather.SUNNY:
+				currentWeather = Weather.SUNNY;
+				this.snow.Stop ();
+				break;
+			case Weather.RAIN:
+				currentWeather = Weather.RAIN;
+				this.rain.Play ();
+				break;
+			case Weather.SNOW:
+				currentWeather = Weather.SNOW;
+				this.rain.Stop ();
+				this.snow.Play ();
+				break;
+			}
+		}
+	}
+
+	private void LerpLightColor (Light light, Color c)
+	{
+		light.color = Color.Lerp (light.color, c, 0.2f * Time.deltaTime);
+	}
+
+	private void LerpSunIntensity(Light light, float intensity)
+	{
+		light.intensity = Mathf.Lerp (light.intensity, intensity, 0.2f * Time.deltaTime);
+	}
+
+	private void Update()
+	{
+		this.Weather_time -= Time.deltaTime;
+
+		if (this.currentWeather == Weather.SUNNY) {
+			LerpSunIntensity (this.sunLight, sunnylightintensity);
+			LerpLightColor (this.sunLight, sunnyColor);
+			if (this.Weather_time <= 0f) {
+				ChangeWeather (Weather.RAIN);
+				day++;
+				Weather_time = Origin_time;
+			}
+		}
+		if (this.currentWeather == Weather.RAIN) {
+			LerpSunIntensity (this.sunLight, rainlightintensity);
+			LerpLightColor (this.sunLight, rainColor);
+			if (this.Weather_time <= 0f) {
+				ChangeWeather (Weather.SNOW);
+				day++;
+				Weather_time = Origin_time;
+			}
+		}
+
+		if (this.currentWeather == Weather.SNOW) {
+			LerpSunIntensity (this.sunLight, snowlightintensity);
+			LerpLightColor (this.sunLight, snowColor);
+			if (this.Weather_time <= 0f) {
+				ChangeWeather (Weather.SUNNY);
+				day++;
+				Weather_time = Origin_time;
+			}
+		}
+
+	}
+
+}
