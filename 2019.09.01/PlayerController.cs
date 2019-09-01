845c7ec8ae27278c3bb12973e5c0ff43e7f212a4
dayeondayeon <50194490+dayeondayeon@users.noreply.github.com>
2019-09-01 19:22:19 +0900

    Add files via upload

---
+++ 2019.09.01/PlayerController.cs
@@ -0,0 +1,215 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float h = 0.0f;
+    private float v = 0.0f;
+    private float r = 0.0f;
+
+    private Transform tr;
+    public float moveSpeed = 10.0f;
+    public float rotSpeed = 80.0f;
+    public float jumpPower = 10.0f;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        r = Input.GetAxis("Mouse X");
+
+        Debug.Log("h=" + h.ToString());
+        Debug.Log("v=" + v.ToString());
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+        tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r);
+    }
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private float h = 0.0f;
+    private float v = 0.0f;
+    private float r = 0.0f;
+    private float s = 0.0f;
+    private Transform tr;
+    public float moveSpeed = 3.0f;
+    public float rotSpeed = 80.0f;
+    public float jumpPower = 10.0f;
+    Animator animator;
+
+    private GameObject closest_item = null;
+    private GameObject carried_item = null;
+    private ItemRoot item_root = null;
+    public GUIStyle guistyle;
+
+    private struct Key
+    {
+        public bool pick;
+        public bool action;
+    }
+
+    private Key key;
+
+    void Start()
+    {
+       tr = GetComponent<Transform>();
+       animator = GetComponent<Animator>();
+       //추가, 다연
+       this.item_root = GameObject.Find("Food").GetComponent<ItemRoot>();
+
+       this.guistyle.fontSize = 14; 
+    }
+
+    private void get_input()
+    {
+        this.key.pick = Input.GetKeyDown(KeyCode.Z);
+        
+    }
+
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        r = Input.GetAxis("Mouse X");
+        s = Input.GetAxis("Fire3");
+        tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r);
+
+        //움직이지 않을때
+        if (h == 0 && v == 0) animator.SetBool("Ismove", false);
+
+        //움직일떄
+        else
+        {
+            //쉬프트를 누르지 않았을 떄
+            if (s == 0)
+            {
+                moveSpeed = 3.0f;
+                animator.SetBool("Isrun", false);
+            }
+            //쉬프트를 눌렀을 때
+            else
+            {
+                moveSpeed = 5.0f;
+                animator.SetBool("Isrun", true);
+            }
+            animator.SetBool("Ismove", true);
+            Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+            tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+        }
+        //여기서 부터 추가한 부분------------다연//
+        this.get_input();
+        this.pick_or_drop_control(); // 함수 이름 수정함, 2019.09.01, 다연
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        GameObject other_go = other.gameObject;
+        if (other_go.layer == LayerMask.NameToLayer("Item"))
+        {
+            if (this.closest_item == null)
+            {
+                if (this.is_other_in_view(other_go))
+                {
+                    this.closest_item = other_go;
+                }
+            }
+            else if (this.closest_item == other_go)
+            {
+                if (!this.is_other_in_view(other_go))
+                {
+                    this.closest_item = null;
+                }
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(this.closest_item == other.gameObject)
+        {
+            this.closest_item = null;
+        }
+    }
+
+    //gui함수 9.1 추가, 다연 //
+    void OnGUI()
+    {
+        
+        if(this.carried_item != null)
+        {
+            GUI.Box(new Rect(360, 200, 100, 40), "Choose");
+            GUI.Button(new Rect(360, 230, 100, 90), "Z : 버린다", guistyle);
+        }
+        else
+        {
+            if(this.closest_item != null)
+            {
+                GUI.Box(new Rect(340 ,210 , 100, 40), "Choose");
+                GUI.Button(new Rect(360, 230, 100, 90), "Z : 줍는다", guistyle);
+            }
+        }
+    }
+
+    private void pick_or_drop_control() //함수 이름 수정, 2019.09.01
+    {
+        do
+        {
+            if (!this.key.pick)
+            {
+                break;
+            }
+            else
+            {
+                if (this.carried_item == null)
+                {
+                    if (this.closest_item == null)
+                    {
+                        break;
+                    }
+
+                    this.carried_item = this.closest_item;
+                    GameObject.Destroy(this.carried_item);
+                    this.closest_item = null;
+                }
+
+                else
+                {
+                    this.carried_item = null;
+                }
+            }
+        } while (false);
+    }
+    
+    private bool is_other_in_view(GameObject other)
+    {
+        bool ret = false;
+        do
+        {
+            Vector3 heading = this.transform.TransformDirection(Vector3.forward);
+            Vector3 to_other = other.transform.position - this.transform.position;
+            heading.y = 0.0f;
+            to_other.y = 0.0f;
+            heading.Normalize();
+            to_other.Normalize();
+            float dp = Vector3.Dot(heading, to_other);
+            if (dp < Mathf.Cos(45.0f))
+            {
+                break;
+            }
+            ret = true;
+        } while (false);
+        return ret;
+    }
+}
---
+++ 2019.09.01/WeatherManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour {
+	public enum Weather { NONE, SUNNY, RAIN, SNOW };
+	public Weather currentWeather;
+
+	public ParticleSystem rain;
+	public ParticleSystem snow;
+
+	[Header("Time Setting")]
+	[Header("Day")]
+
+	public float Weather_time;
+    //8.21추가한 랜덤용 인수//
+    public int Next_Weather;
+	public int day;
+	private float Origin_time = 10f;
+
+	[Header("LightSetting")] //날씨따라 햇빛강도 다르게//
+	public Light sunLight;
+	private float defaultLightIntensity;
+	public float sunnylightintensity;
+	public float rainlightintensity;
+	public float snowlightintensity;
+
+	public Color defaultLightColor;
+	public Color sunnyColor;
+	public Color rainColor;
+	public Color snowColor;
+
+	private void Start()
+	{
+		currentWeather = Weather.SUNNY;
+        Next_Weather = 0;
+		this.defaultLightColor = this.sunLight.color;
+		this.defaultLightIntensity = this.sunLight.intensity;
+		day = 1;
+	}
+
+	public void ChangeWeather(Weather weatherType)
+	{
+		if(weatherType != this.currentWeather)
+		{
+			switch (weatherType)
+			{
+			case Weather.SUNNY:
+				currentWeather = Weather.SUNNY;
+				this.snow.Stop ();
+                this.rain.Stop();
+				break;
+			case Weather.RAIN:
+				currentWeather = Weather.RAIN;
+                this.snow.Stop();
+				this.rain.Play ();
+				break;
+			case Weather.SNOW:
+				currentWeather = Weather.SNOW;
+				this.rain.Stop ();
+				this.snow.Play ();
+				break;
+			}
+		}
+	}
+
+	private void LerpLightColor (Light light, Color c)
+	{
+		light.color = Color.Lerp (light.color, c, 0.2f * Time.deltaTime);
+	}
+
+	private void LerpSunIntensity(Light light, float intensity)
+	{
+		light.intensity = Mathf.Lerp (light.intensity, intensity, 0.2f * Time.deltaTime);
+	}
+
+	private void Update()
+	{
+		this.Weather_time -= Time.deltaTime;
+        
+
+		//if (this.currentWeather == Weather.SUNNY)
+        if(Next_Weather >= 0 && Next_Weather < 6)
+        {
+			LerpSunIntensity (this.sunLight, sunnylightintensity);
+			LerpLightColor (this.sunLight, sunnyColor);
+			if (this.Weather_time <= 0f) {
+                Next_Weather = Random.Range(0, 10);
+				ChangeWeather (Weather.SUNNY);
+				day++;
+				Weather_time = Origin_time;
+            }
+		}
+		//if (this.currentWeather == Weather.RAIN)
+        if (Next_Weather >= 6 && Next_Weather < 8)
+        {
+			LerpSunIntensity (this.sunLight, rainlightintensity);
+			LerpLightColor (this.sunLight, rainColor);
+			if (this.Weather_time <= 0f) {
+                Next_Weather = Random.Range(0, 10);
+                ChangeWeather (Weather.RAIN);
+				day++;
+				Weather_time = Origin_time;
+            }
+		}
+
+		//if (this.currentWeather == Weather.SNOW)
+        if (Next_Weather >= 8 && Next_Weather < 10)
+        {
+			LerpSunIntensity (this.sunLight, snowlightintensity);
+			LerpLightColor (this.sunLight, snowColor);
+			if (this.Weather_time <= 0f) {
+                Next_Weather = Random.Range(0, 10);
+                ChangeWeather (Weather.SNOW);
+                day++;
+                Weather_time = Origin_time;
+            }
+		}
+
+	}
+
+}
