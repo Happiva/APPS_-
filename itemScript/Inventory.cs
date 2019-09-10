b07739d053659dc064334aa71587304f52823ac1
Happiva <48710066+Happiva@users.noreply.github.com>
2019-09-10 22:26:36 +0900

    Add files via upload

---
+++ itemScript/Inventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public int[] amount;
+    public Text[] amountText;
+    public GameObject[] slots;
+}
---
+++ itemScript/PickUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+    private Inventory inventory;
+
+    private void Start() {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+    /*
+    public void PutInInventory(GameObject item) {
+        if (item.CompareTag("Potato"))
+        {
+            inventory.amount[0]++;
+        }
+        else if (item.CompareTag("Crab"))
+        {
+            inventory.amount[1]++;
+        }
+        else if (item.CompareTag("Tree"))
+        {
+            inventory.amount[2]++;
+        }
+    }
+    */
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            inventory.amount[0]++;
+            inventory.amountText[0].text = inventory.amount[0] + "";
+        }
+    }
+}
