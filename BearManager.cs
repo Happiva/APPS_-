7782ed5ecf7df23aaf7cf3493e8c5a794e949c01
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2019-09-18 11:48:38 +0900

    Add files via upload

---
+++ BearManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearManager : MonoBehaviour
+{
+    public Transform point;
+    public GameObject BearPrefab;
+    public float createTime;
+    public int Maxbear = 1;
+    public bool isGameOver = false;
+
+    private void Start()
+    {
+        point = GameObject.Find("Bear_point").GetComponentInChildren<Transform>();
+
+        if (point != null)
+        {
+            StartCoroutine(this.CreateBear());
+        }
+    }
+    IEnumerator CreateBear()
+    {
+        while (!isGameOver)
+        {
+            int BearCount = (int)GameObject.FindGameObjectsWithTag("Bear").Length;
+            if (BearCount < Maxbear)
+            {
+                yield return new WaitForSeconds(createTime);
+                Instantiate(BearPrefab, point.position, point.rotation);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+    public void Dead()
+    {
+
+    }
+
+}
